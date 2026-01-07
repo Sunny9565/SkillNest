20b4eefe4e8584f6c86ee214519a23e1189de9d8
@sunny.gupta <sunny.gupta@bdb.ai>
2026-01-08 00:24:05 +0530

    Master Branch

---
+++ src/app/shared/navbar/navbar.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+ menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
