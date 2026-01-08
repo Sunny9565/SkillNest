38c24bcc9e58e566dc4b73be388b11a8046e6063
@sunny.gupta <sunny.gupta@bdb.ai>
2026-01-08 18:09:05 +0530

    Login Form Bootstrap Installion

--- src/app/layout/layout/layout.ts
+++ src/app/layout/layout/layout.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Navbar} from '../.././shared/navbar/navbar'
-
+import { Navbar } from '../.././shared/navbar/navbar';
+import { MatDialog } from '@angular/material/dialog';
+import { Login } from '../../login/login/login';
 @Component({
   selector: 'app-layout',
   imports: [RouterOutlet, Navbar],
@@ -9,5 +10,19 @@ import {Navbar} from '../.././shared/navbar/navbar'
   styleUrl: './layout.css',
 })
 export class Layout {
+  constructor(private dialog: MatDialog) {}
+
+ openLogin() {
+  this.dialog.open(Login, {
+    width: '1132px',        
+    maxWidth: '90vw',    
+    height: '70vh',
+    maxHeight: '70vh',
+    disableClose: true,
+    panelClass: 'center-login-dialog'
+  });
+
+  
+}
 
 }
